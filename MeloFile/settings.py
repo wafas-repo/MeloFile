a66b5da4772472519fcf3747f3baba5dfdd5086e
wafas-repo <wafa.qazi@outlook.com>
2023-03-23 02:43:30 -0400

    changes made for deployment

--- MeloFile/settings.py
+++ MeloFile/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['melofile.up.railway.app','https://*.127.0.0.1']
 
 
 # Application definition
